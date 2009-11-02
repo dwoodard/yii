dwoodard/yii

commit 824f7c62eb858ba1e8317d3c9637ddeb6af5d034

(Fixes issue 643)
git-svn-id: http://yii.googlecode.com/svn/branches/1.0@1480 c7f931c7-7552-0410-b027-2fb3d89d9100

framework/caching/dependencies/CExpressionDependency.php
@@ -25,6 +25,10 @@ class CExpressionDependency extends CCacheDependency
 {
 	/**
 	 * @var string the PHP expression whose result is used to determine the dependency.
+	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
+	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
+	 * parameter which is the dependency object itself.
 	 */
 	public $expression;
 
@@ -44,6 +48,9 @@ class CExpressionDependency extends CCacheDependency
 	 */
 	protected function generateDependentData()
 	{
-		return @eval('return '.$this->expression.';');
+		if(is_callable($this->expression))
+			return call_user_func($this->expression, $this);
+		else
+			return @eval('return '.$this->expression.';');
 	}
 }

framework/web/auth/CAccessControlFilter.php
@@ -172,6 +172,10 @@ class CAccessRule extends CComponent
 	/**
 	 * @var string a PHP expression whose value indicates whether this rule should be applied.
 	 * In this expression, you can use <code>$user</code> which refers to <code>Yii::app()->user</code>.
+	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
+	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
+	 * parameter which is the user object.
 	 * @since 1.0.3
 	 */
 	public $expression;
@@ -290,6 +294,9 @@ class CAccessRule extends CComponent
 	{
 		if($this->expression===null)
 			return true;
-		return @eval('return '.$this->expression.';');
+		if(is_callable($this->expression))
+			return call_user_func($this->expression, $user);
+		else
+			return @eval('return '.$this->expression.';');
 	}
 }

framework/web/widgets/COutputCache.php
@@ -95,6 +95,10 @@ class COutputCache extends CFilterWidget
 	 * @var string a PHP expression whose result is used in the cache key calculation.
 	 * By setting this property, the output cache will use different cached data
 	 * for each different expression result.
+	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
+	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
+	 * parameter which is the output cache object itself.
 	 * @since 1.0.4
 	 */
 	public $varyByExpression;
@@ -289,7 +293,10 @@ class COutputCache extends CFilterWidget
 
 	private function evaluateExpression($expression)
 	{
-		return @eval('return '.$expression.';');
+		if(is_callable($expression))
+			return call_user_func($expression, $this);
+		else
+			return @eval('return '.$expression.';');
 	}
 
 	/**

commit 107c0d20ad087940f4461fc767d53cecd23e9bc4

fixed bugs introduced by r1480
git-svn-id: http://yii.googlecode.com/svn/branches/1.0@1507 c7f931c7-7552-0410-b027-2fb3d89d9100

framework/caching/dependencies/CExpressionDependency.php
@@ -26,7 +26,7 @@ class CExpressionDependency extends CCacheDependency
 	/**
 	 * @var string the PHP expression whose result is used to determine the dependency.
 	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
-	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * including class method name (array(ClassName/Object, MethodName)),
 	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
 	 * parameter which is the dependency object itself.
 	 */
@@ -48,7 +48,7 @@ class CExpressionDependency extends CCacheDependency
 	 */
 	protected function generateDependentData()
 	{
-		if(is_callable($this->expression))
+		if(!is_string($this->expression) && is_callable($this->expression))
 			return call_user_func($this->expression, $this);
 		else
 			return @eval('return '.$this->expression.';');

framework/web/auth/CAccessControlFilter.php
@@ -173,7 +173,7 @@ class CAccessRule extends CComponent
 	 * @var string a PHP expression whose value indicates whether this rule should be applied.
 	 * In this expression, you can use <code>$user</code> which refers to <code>Yii::app()->user</code>.
 	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
-	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * including class method name (array(ClassName/Object, MethodName)),
 	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
 	 * parameter which is the user object.
 	 * @since 1.0.3
@@ -294,7 +294,7 @@ class CAccessRule extends CComponent
 	{
 		if($this->expression===null)
 			return true;
-		if(is_callable($this->expression))
+		if(!is_string($this->expression) && is_callable($this->expression))
 			return call_user_func($this->expression, $user);
 		else
 			return @eval('return '.$this->expression.';');

framework/web/widgets/COutputCache.php
@@ -96,7 +96,7 @@ class COutputCache extends CFilterWidget
 	 * By setting this property, the output cache will use different cached data
 	 * for each different expression result.
 	 * Starting from version 1.0.11, the expression can also be a valid PHP callback,
-	 * including function name, class method name (array(ClassName/Object, MethodName)),
+	 * including class method name (array(ClassName/Object, MethodName)),
 	 * or anonymous function (PHP 5.3.0+). The function/method will be passed a single
 	 * parameter which is the output cache object itself.
 	 * @since 1.0.4
@@ -293,7 +293,7 @@ class COutputCache extends CFilterWidget
 
 	private function evaluateExpression($expression)
 	{
-		if(is_callable($expression))
+		if(!is_string($expression) && is_callable($expression))
 			return call_user_func($expression, $this);
 		else
 			return @eval('return '.$expression.';');